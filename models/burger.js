865c901f702c9f360269c85fbd8786d41a90216d
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-05-11 22:42:04 -0500

    edited comments

--- models/burger.js
+++ models/burger.js
@@ -24,23 +24,3 @@ var burger = {
 module.exports = burger;
 
 
-// var orm = require('../config/orm.js');
-
-// var burger = {
-//     selectAll: function (cb) {
-//         orm.selectAll('burgers', function (res) {
-//             cb(res);
-//         });
-//     },
-
-//     // insertOne: 
-//     // },
-
-//     updateOne: function (tableInput, col, valOfCol) {
-//         orm.insertOne("burgers", tableInput, col, valOfCol, function (res) {
-//             cb(res);
-//         });
-//     }
-// };
-
-// module.exports = burger;
